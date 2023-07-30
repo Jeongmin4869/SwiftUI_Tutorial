301189e1344729230523a1f100ed3250c439d026
jeongmin4869 <ljm4135@naver.com>
2023-07-30 19:32:14 +0900

    Initial Commit

---
+++ Landmakrs/LandmakrsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmakrsApp.swift
+//  Landmakrs
+//
+//  Created by 이정민 on 2023/07/30.
+//
+
+import SwiftUI
+
+@main
+struct LandmakrsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
